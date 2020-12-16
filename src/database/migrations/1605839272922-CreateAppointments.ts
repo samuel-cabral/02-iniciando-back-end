d961f6da40cf925dca7fbb252e1a8f7bed569254
Samuel Cabral <samuelcabral.mail@gmail.com>
2020-12-16 20:42:04 -0300

    finish: modulo banco de dados

---
+++ src/database/migrations/1605839272922-CreateAppointments.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1605839272922 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone', // if does not use postgres, set this 'timestamp' only.
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
